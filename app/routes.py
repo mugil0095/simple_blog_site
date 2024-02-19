50f832903c5b515e7e2654f749659289d4f21f5d
Ilamugil Balasubramaniam <ilamugil400300@gmail.com>
2024-02-19 14:01:30 +0530

    Add files via upload

---
+++ app/routes.py
@@ -0,0 +1,41 @@
+from flask import render_template, request, redirect, url_for
+from app import app, db
+from app.models import Post
+
+@app.route('/')
+def index():
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('show_post.html', post=post)
+
+@app.route('/post/create', methods=['GET', 'POST'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_post = Post(title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('create_post.html')
+
+@app.route('/post/<int:post_id>/edit', methods=['GET', 'POST'])
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('show_post', post_id=post.id))
+    return render_template('edit_post.html', post=post)
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('index'))
